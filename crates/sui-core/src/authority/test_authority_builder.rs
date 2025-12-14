d5141d7b7ec8356b448e9da23ced431a286292d6
mcoba891120 <mcoba891120@gmail.com>
2025-12-14 21:14:13 +0800

    feat: add optional custom broadcaster to authority state and refactor its websocket message handling for Axum API updates.

--- crates/sui-core/src/authority/test_authority_builder.rs
+++ crates/sui-core/src/authority/test_authority_builder.rs
@@ -263,6 +263,7 @@ impl<'a> TestAuthorityBuilder<'a> {
             &registry,
             &authority_store,
             backpressure_manager.clone(),
+            None,
         );
 
         let chain_id = ChainIdentifier::from(*genesis.checkpoint().digest());
